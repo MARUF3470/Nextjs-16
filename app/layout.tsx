64c566051eb5d2a7f5285e7c7de1d18fd198d6db
Maruf3470 <smmaruf25@gmail.com>
2026-04-01 15:00:37 +1100

    exploring nextjs 16

---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import LightRays from "@/components/LightRays";
+import NavBar from "@/components/NavBar";
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+
+const schibsted_Grotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted_Grotesk",
+  subsets: ["latin"],
+});
+
+const martian_Mono = Martian_Mono({
+  variable: "--font-martian_Mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The hub for Every Dev Event You Mustn't miss",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html suppressHydrationWarning={true}
+      lang="en"
+      className={cn("antialiased", schibsted_Grotesk.variable, martian_Mono.variable, "font-sans", geist.variable)}
+    >
+      <body className="min-h-screen flex flex-col">
+        <NavBar />
+        <div className="absolute inset-0 z-[-1] top-0 min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={1}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0.01}
+          />
+        </div>
+        <main>
+
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ components/NavBar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <Link href="/" className="logo"><Image src="/icons/logo.png" alt="Logo" width={24} height={24} /><p>DevEvent</p></Link>
+                <ul>
+                    <Link href="/">Home</Link>
+                    <Link href="/events">Events</Link>
+                    <Link href="/create-event">Create Event</Link>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
